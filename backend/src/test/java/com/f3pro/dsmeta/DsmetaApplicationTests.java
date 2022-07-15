2f7a6020488574e0798efa08f4d61d961ce20f65
Francivaldo Alves <francivaldo.alves@gmail.com>
2022-07-15 19:53:41 -0300

    Project created

---
+++ backend/src/test/java/com/f3pro/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f3pro.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
