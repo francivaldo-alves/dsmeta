c9ad9c5689ee90aaa3dc72d7b8abac294116505a
Francivaldo Alves <francivaldo.alves@gmail.com>
2022-07-16 16:01:26 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,25 @@
-import NotificationButton from "./components/botificationButton"
 
-import './components/botificationButton/styles.css'
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
+
 
 
 function App() {
   return (
-  <>
-    <h1> Ola mundo</h1>
-    <NotificationButton />
+    <>
+      <Header />
+      <main>
+      <section id="sales">
+          <div className="dsmeta-container">
+        <SalesCard />
+        </div>
+        </section>
+      </main>
 
 
-  </>
+
+
+    </>
   )
 }
 
